51bc798d5ee81c2f71dc6b49044bd856b454fc63
kasonyang <me@kason.site>
2017-09-23 17:20:00 +0800

    do inspect

--- src/main/java/site/kason/myinspect/MyinspectProcessor.java
+++ src/main/java/site/kason/myinspect/MyinspectProcessor.java
@@ -2,6 +2,8 @@ package site.kason.myinspect;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import javax.annotation.processing.AbstractProcessor;
 import javax.annotation.processing.RoundEnvironment;
@@ -10,6 +12,10 @@ import javax.annotation.processing.SupportedSourceVersion;
 import javax.lang.model.SourceVersion;
 import javax.lang.model.element.Element;
 import javax.lang.model.element.TypeElement;
+import site.kason.myinspect.inspect.Diagnosis;
+import site.kason.myinspect.inspect.Inspector;
+import site.kason.myinspect.inspect.SyntaxAnalyzer;
+import site.kason.myinspect.util.MybatisSQLUtil;
 
 /**
  *
@@ -26,17 +32,38 @@ public class MyinspectProcessor extends AbstractProcessor {
     for (Element e : roundEnv.getRootElements()) {
       srev.visit(e);
     }
+    Result result = srev.getResult();
+    Map<String, ResultGroup> resultMap = result.getResults();
+    Inspector inspector = new Inspector();
+    String jdbcUrl = System.getProperty("myinspect.db.url");
+    String user = System.getProperty("myinspect.db.user");
+    String password = System.getProperty("myinspect.db.password");
+    if(jdbcUrl!=null && !jdbcUrl.isEmpty()){
+      inspector.addAnalyzer(new SyntaxAnalyzer(jdbcUrl, user, password));
+    }
+    for(ResultGroup rg :resultMap.values()){
+      for(ResultItem it:rg.getItems()){
+        String sql = it.getSql();
+        String normalSql = MybatisSQLUtil.removeVar(sql);
+        List<Diagnosis> diagnosisList = inspector.inspect(normalSql);
+        for(Diagnosis d:diagnosisList){
+          System.out.println(sql);
+          System.out.println(d);
+          System.out.println("");
+        }
+      }
+    }
     
     String outFile = System.getProperty("myinspect.outfile");
     if (outFile != null && !outFile.isEmpty()) {
       try {
-        String out = srev.getResult().render();
+        String out = result.render();
         //System.out.println(out);
         System.out.println(out.length());
         byte[] bytes = out.getBytes("utf-8");
-        System.out.println(bytes.length);
+        //System.out.println(bytes.length);
         try (FileOutputStream fos = new FileOutputStream(outFile)) {
-          fos.write(out.getBytes("utf-8"));
+          fos.write(bytes);
           //FileUtils.writeStringToFile(new File(outFile), out, "utf-8");
         }
       } catch (IOException ex) {
